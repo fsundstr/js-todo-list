1eb7fc52cb3ab13fcf53482e32a90c7fc9d0ddc2
fredstr <fredrik.sundstromm@gmail.com>
2025-08-18 09:50:29 +0200

    Added functionality for new weeks and navigation
    Added feature: it is now possible to create a new week in the task
    schedule and navigate between weeks.

--- script.js
+++ script.js
@@ -1,7 +1,101 @@
 
+let weekTemplate = document.getElementById("main").cloneNode(true);
+let pages = [document.getElementById("main")];
+let index =0;
+let nPages = 1;
+
 let items = [];
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".header .right td").addEventListener("click", function() {
+        navigateToPage(this);
+    });
+});
+
+
+// @Input: td HTML element 
+function navigateToPage(pageNumber){
+    let number = Number(pageNumber.textContent);
+    /*
+    console.log(number);
+    console.log("test"); */
+    let page = document.getElementById("main");
+    page.replaceWith(pages[number-1]);
+    index = number-1;
+    /*printPagesList();*/
+
+}
+// adds a navigation number to the UI
+// @Input global variable nPages 
+function addNavigationNumber(pageNumber){
+    let navigationRow = document.querySelector(".header .right .navigation tr");
+    let newNumber = document.createElement('td');
+    newNumber.textContent = String(pageNumber);
+    newNumber.addEventListener("click", function() {
+        navigateToPage(this);
+    });
+    navigationRow.appendChild(newNumber);
+}
+
+
+
+// Adding a new middle section and 
+function addPage(){
+    pages.push(weekTemplate.cloneNode(true));
+    nPages++;
+    addNavigationNumber(nPages);
+
+}
+
+
+function nextPageRight(){
+    if(index+1 === nPages){
+        addPage();
+        let page = document.getElementById("main");
+        page.replaceWith(pages[index+1]);
+        index++;
+        /*
+        console.log("page added");
+        console.log(`index: ${index}, nPages: ${nPages} `);*/
+   
+       
+    } else{
+      // display the next page
+        let page = document.getElementById("main");
+        page.replaceWith(pages[index+1]);
+        index++;
+        /*
+        console.log("show next page");
+        console.log(`index: ${index}, nPages: ${nPages} `);*/
+
+    }
+
+}
+
+function nextPageLeft(){
+    if(index === 0){
+        /*
+        console.log("This is the first page, not possible to add pages to the left");
+        console.log(`index: ${index}, nPages: ${nPages} `); */
+        return;
+    } else{
+        let page = document.getElementById("main");
+        page.replaceWith(pages[index-1]);
+        index--;
+        /*
+        console.log("show previous page");
+        console.log(`index: ${index}, nPages: ${nPages} `); */
+    }
+         
+    
+
+}
+
+
+
+
+// Functionality to add a text field
 function addTask(day){
     let taskList = document.getElementById(day+"List");
     let listItem = document.createElement('li');
@@ -9,7 +103,7 @@ function addTask(day){
     textArea.autocomplete = 'on';
     textArea.rows = '5';
     textArea.cols = '22';
-    textArea.ondblclick = function(){textFieldEvent(listItem)};
+    textArea.ondblclick = function(){textFieldEventDelete(listItem)};
 
     listItem.appendChild(textArea);
     taskList.appendChild(listItem);
@@ -17,17 +111,24 @@ function addTask(day){
 
 }
 
-function textFieldEvent(listItem){
+// Eventhandler to remove a text field
+function textFieldEventDelete(listItem){
 
     items.push(listItem);
     listItem.remove();
-    printList();
+    printPagesList();
 
 }
 
-
-function printList(){
+// For testing purposes
+function printTextareaList(){
     for(let element in items){
         console.log(element + ": " + items[element].querySelector('textarea').value);
+    }
+}
+
+function printPagesList(){
+    for(let element in pages){
+        console.log(element + ": " + pages)
     }
 }
